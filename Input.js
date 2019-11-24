f16f6044789a84b12fda6d502ca64af22e9c72b7
Yuka <yuka.0531.a@gmail.com>
2019-11-23 19:37:28 -0800

    add files that we covered in seminar01

---
+++ Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+import AppState from "./AppState";
+
+const Input = () => {
+  let { input, onInputChange } = AppState.useContainer();
+
+  return (
+    <div className="input">
+      <span>A1.</span>
+      <input type="text" value={input} onChange={onInputChange}></input>
+      {input}
+    </div>
+  );
+};
+
+export default Input;
---
+++ Title.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <h1>Score or Die</h1>;
